3cef4114f515b679fc3af187993cec7b3ee2e43f
Ankush chalotra <30835152+ankushchalotra11071994@users.noreply.github.com>
2021-01-24 18:31:32 +0530

    crud

---
+++ Ecom/Ecom.Services/EcomServices.cs
@@ -0,0 +1,58 @@
+﻿using Ecom.Database;
+using Ecom.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Services
+{
+   
+  public class EcomServices
+    {
+        EcomContext ecom = new EcomContext();
+        public EcomServices()
+        {
+
+        }
+
+        public  List<Category> GetCategories()
+        {
+            return ecom.Categories.ToList();
+        }
+        public void addcategory ( Category category)
+        {
+            ecom.Categories.Add(category);
+            ecom.SaveChanges();
+        }
+        public void EditCategory(Category category)
+        {
+            //ecom.Entry(category).State=EntityState.Modified;
+            //ecom.SaveChanges();
+
+            Category categoryobj = ecom.Categories.ToList()
+                .Where(x => x.categoryId == category.categoryId).FirstOrDefault();
+            if (categoryobj != null)
+            {
+                categoryobj.categoryId = (int)category.categoryId;
+                categoryobj.Name = category.Name;
+                categoryobj.Description = category.Description;
+                ecom.SaveChanges();
+            }
+        }
+        public Category GetCategorybyid ( int id)
+        {
+          return  ecom.Categories.Find(id);
+        }
+        public bool Deletebyid ( int id)
+        {
+            Category categoryobj = ecom.Categories.ToList()
+               .Where(x => x.categoryId == id).FirstOrDefault();
+           var check= ecom.Categories.Remove(categoryobj);
+            ecom.SaveChanges();
+            return true;
+        }
+    }
+}
---
+++ Ecom/Ecom.Web/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using Ecom.Database;
+using Ecom.Entities;
+using Ecom.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecom.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        EcomServices services = new EcomServices();
+        
+        public ActionResult Index()
+        {
+            
+            return View(services.GetCategories());
+        }
+   
+        public ViewResult Create()
+        {
+            return View();
+        }
+        [HttpPost] 
+        public ActionResult Create( Category category)
+        {
+            services.addcategory(category);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            
+            return View(services.GetCategorybyid(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+
+            services.EditCategory(category);
+             return RedirectToAction("Index");
+        }
+        
+        
+        public ActionResult Delete(int id)
+        {
+
+            services.Deletebyid(id);
+             return RedirectToAction("Index");
+        }
+    }
+}
